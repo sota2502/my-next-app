e2a100de8f3963695645e0cb935e9c90d4178ca0
sota2502 <sota2502@users.noreply.github.com>
2024-10-02 00:03:23 +0900

    Add prisma settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { hostname: 'books.google.com' },
+    ]
+  }
+};
 
 export default nextConfig;
